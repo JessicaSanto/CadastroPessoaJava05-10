78eaac786b9461fc1fd05481e5f9354aa34a51ca
JessicaSanto <jessicaesanto@outlook.com>
2023-10-06 20:20:07 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,65 @@ public class Main {
         System.out.println("Bem vindo ao sistema de cadastro de Pessoa Física e Pessoa Jurídica");
 
         Scanner scanner = new Scanner(System.in);
+        int opcao;
 
         do {
 
-            System.out.println();
+            System.out.println("Escolha uma opção: 1 - Pessoa Física / 2 - Pessoa Jurídica / 0 - Sair");
+            opcao = scanner.nextInt();
 
-        } while ();
+            switch (opcao) {
+                case 1:
+                    int opcaoPf;
+                    do {
+
+                        System.out.println("Escolha uma opção: 1 - Cadastrar Pessoa Física / 2 - Listar Pessoa Física / 0 - Voltar ao menu anterior ");
+                        opcaoPf = scanner.nextInt();
+
+                        switch (opcaoPf){
+                            case 1:
+                                PessoaFisica novapf = new PessoaFisica();
+
+                                System.out.println("Digite o nome da pessoa física: ");
+                                novapf.nome = scanner.next();
+
+                                System.out.println("Digite o CPF: ");
+                                novapf.cpf = scanner.next();
+
+                                System.out.println("Digite o rendimento mensal (Digite somente numero): ");
+                                novapf.rendimento = scanner.nextInt();
+
+                                System.out.println("Digite a data de Nascimento (dd/MM/aaaa): ");
+
+                                break;
+                            case 2:
+                                break;
+                            case 0:
+                                System.out.println("Voltando ao menu anterior");
+                                break;
+                            default:
+                                System.out.println("Opção inválida, por favor digite uma opção válida!");
+                                break;
+                        }
+
+                    } while (opcaoPf != 0);
+
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    System.out.println("Obrigado por utilizar o nosso sistema! Forte abraço! ");
+                    break;
+                default:
+                    System.out.println("Opção inválida, por favor digite uma opção válida!");
+                    break;
+            }
+
+        } while (opcao != 0);
     }
 }
+
+
+
+
+
