d8a586d67cd9b838da363a1a0d33e3b3af4ef667
JessicaSanto <jessicaesanto@outlook.com>
2023-10-06 18:53:59 -0300

    m

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bem vindo ao sistema de cadastro de Pessoa Física e Pessoa Jurídica");
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+
+            System.out.println();
+
+        } while ();
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa {
+    public String nome;
+    public float rendimento;
+    public Endereco endereco;
+    public float CalcularImposto(float rendimento){
+    return rendimento;
+    }
+}
